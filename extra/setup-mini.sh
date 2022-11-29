454fcc008de540809f13ed61ca03fdda0daa7037
bcl <bcl@nclasters.org>
2022-11-28 23:44:05 -0500

    update setup script

--- extra/setup-mini.sh
+++ extra/setup-mini.sh
@@ -1,6 +1,6 @@
 sudo minikube delete
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.26.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
-sudo minikube start --vm-driver=none --kubernetes-version=1.23.0
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.28.0/minikube-linux-amd64 && sudo chmod +x minikube && sudo cp minikube /usr/local/bin/ && sudo rm minikube
+sudo minikube start --vm-driver=none --kubernetes-version=1.23.1
 sudo mv /home/diyuser3/.kube /home/diyuser3/.minikube $HOME
 sudo chown -R $USER $HOME/.kube $HOME/.minikube
 sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
